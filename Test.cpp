5cb948a1dc61b4bb4e68049f54dd333f7c18f1b6
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-04-28 18:38:41 +0300

    done testing

--- Test.cpp
+++ Test.cpp
@@ -75,95 +75,117 @@ TEST_CASE("Test real equations /") {
     CHECK(solve(1 == x / 2) == 2.0);
     CHECK(solve(10 == x / 10) == 100.0);
     CHECK(solve(-10 == x / 10) == -100.0);
-    // CHECK_THROWS(solve(10 == 10 / x));
-    // CHECK_THROWS(solve(0 == 10 / x));
 }
 
-TEST_CASE("Test real equations both sides") {
+TEST_CASE("Test real equations both sides ^2") {
     RealVariable x;
-    CHECK(solve(x / 2 == 7) == 14.0);
-    CHECK(solve(x / 10 == 0.05) == 0.5);
-    CHECK(solve(x / 10 == -10) == -100);
-    CHECK(solve(x / 0.2 == 1.2) == 0.24);
-    CHECK(solve(7.5 == x / 5) == 37.5);
-    CHECK(solve(1 == x / 2) == 2.0);
-    CHECK(solve(10 == x / 10) == 100.0);
-    CHECK(solve(-10 == x / 10) == -100.0);
-    // CHECK_THROWS(solve(10 == 10 / x));
-    // CHECK_THROWS(solve(0 == 10 / x));
+    CHECK(solve(x + 1 == 2 * x + 1) == 0.0);
+    CHECK(solve(x - 1 == 2 * x - 1) == 0.0);
+    CHECK((solve((x ^ 2) == x ) == 0.0 || solve((x ^ 2) == x) == 1.0));
+    CHECK((solve((x^2)  ==  x +2) == 2.0 || solve((x ^ 2) == x + 2) == -1.0));
 }
 
-TEST_CASE("Test real equations both sides") {
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
-    CHECK(2==2);
+// test ComplexVariable
+
+TEST_CASE("Test simple equations") {
+    ComplexVariable x;
+    CHECK(solve(x == 5) == 5.0);
+    CHECK(solve(x == -5) == -5.0);
+    CHECK(solve(x == 0) == 0.0);
+    CHECK(solve(x == 0.6) == 0.6);
+    CHECK(solve(1.7 == x) == 1.7);
+    CHECK(solve(1 == x) == 1.0);
+    CHECK(solve(0 == x) == 0.0);
+    CHECK(solve(-10 == x) == -10.0);
+}
+
+TEST_CASE("Test real equations +") {
+    ComplexVariable x;
+    CHECK(solve(x + 2 == 7) == 5.0);
+    CHECK(solve(x + 10 == 5) == -5.0);
+    CHECK(solve(x + 10 == 10) == 0.0);
+    CHECK(solve(x + 0.8 == 1.3) == 0.5);
+    CHECK(solve(1.7 == x + 5.2) == -3.5);
+    CHECK(solve(1 == x + 2) == -1.0);
+    CHECK(solve(10 == x + 10) == 0.0);
+    CHECK(solve(-10 == x + 10) == -20.0);
+    CHECK(solve(10 == 10 + x) == 0.0);
+    CHECK(solve(-10 == 10 + x) == -20.0);
+}
+
+TEST_CASE("Test real equations -") {
+    ComplexVariable x;
+    CHECK(solve(x - 2 == 7) == 9.0);
+    CHECK(solve(x - 10 == 5) == 15.0);
+    CHECK(solve(x - 10 == -10) == 0.0);
+    CHECK(solve(x - 0.7 == 1.3) == 2.0);
+    CHECK(solve(1.7 == x - 5.2) == 6.9);
+    CHECK(solve(1 == x - 2) == 3.0);
+    CHECK(solve(10 == x - 10) == 20.0);
+    CHECK(solve(-10 == x - 10) == 0.0);
+    CHECK(solve(10 == 10 - x) == 0.0);
+    CHECK(solve(-10 == 10 - x) == 20.0);
+}
+
+TEST_CASE("Test real equations *") {
+    ComplexVariable x;
+    CHECK(solve(x * 2 == 7) == 3.5);
+    CHECK(solve(x * 10 == 5) == 0.5);
+    CHECK(solve(x * 10 == -10) == -1.0);
+    CHECK(solve(x * 0.7 == 1.4) == 2.0);
+    CHECK(solve(7.5 == x * 5) == 1.5);
+    CHECK(solve(1 == x * 2) == 0.5);
+    CHECK(solve(10 == x * 10) == 1.0);
+    CHECK(solve(-10 == x * 10) == -1.0);
+    CHECK(solve(10 == 10 * x) == 1.0);
+    CHECK(solve(0 == 10 * x) == 0.0);
+}
+
+TEST_CASE("Test real equations /") {
+    ComplexVariable x;
+    std::complex<double> val;
+    val.real(14);
+    val.imag(0);
+    CHECK(solve(x / 2 == 7) == val);
+    val.real(0.5);
+    val.imag(0);
+    CHECK(solve(x / 10 == 0.05) == val);
+    val.real(-20);
+    val.imag(0);
+    CHECK(solve(x / 2 == -10) == val);
+    val.real(0.6);
+    val.imag(0);
+    CHECK(solve(x / 0.5 == 1.2) == val);
+    val.real(14.5);
+    val.imag(0);
+    CHECK(solve(7.25 == x / 2) == val);
+    val.real(2.0);
+    val.imag(0);
+    CHECK(solve(1 == x / 2) == val);
+    val.real(80);
+    val.imag(0);
+    CHECK(solve(10 == x / 8) == val);
+    val.real(-80);
+    val.imag(0);
+    CHECK(solve(-10 == x / 8) == val);
+}
+
+
+TEST_CASE("Test real equations both sides ^2") {
+    ComplexVariable x;
+    std::complex<double> val ,val2;
+    val.real(0);
+    val.imag(0);
+    CHECK(solve(x + 1 == 2 * x + 1) == val);
+    CHECK(solve(x - 1 == 2 * x - 1) == val);
+    val.real(1.0);
+    val.imag(0);
+    val2.real(0.0);
+    val2.imag(0);
+    CHECK((solve((x ^ 2) == x ) == val || solve((x ^ 2) == x) == val2));
+    val.real(2.0);
+    val.imag(0);
+    val2.real(-1.0);
+    val2.imag(0);
+    CHECK((solve((x^2)  ==  x +2) == val || solve((x ^ 2) == x + 2) == val2));
 }
